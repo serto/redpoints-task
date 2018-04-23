f236e78884b2858fb37c8c5f8aaa229e4b75d0d2
Sertorio Morales <sertomorales@gmail.com>
2018-04-23 12:35:44 +0200

    Detail page. I stop here. I will continue in the afternoon, I have a meeting now.

--- src/components/pages/detail.js
+++ src/components/pages/detail.js
@@ -2,22 +2,66 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
+import { Link } from 'react-router-dom'
+
 import { Header } from '../common/Header'
+
+const API_KEY = '892376643df7e1fd6dc72567108ec2e5'
 
 export default class Detail extends Component {
 
   constructor(props){
     super(props);
+
+    this.state = {
+      movieInfo: {}
+    }
+
+  }
+
+  componentDidMount() {
+    const { id } = this.props.match.params
+    
+    fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=${API_KEY}&language=en-US`)
+      .then(res => res.json())
+      .then(response => {
+        console.log(response)
+        this.setState({movieInfo: response})
+      })
   }
 
   render () {
 
-    const { id } = this.props.match.params
+    const { title, 
+      overview, 
+      poster_path,
+      vote_average,
+      homepage
+      } = this.state.movieInfo
 
     return (
       <div className='wrapper'>
         <Header>RedPoints MovieSearch</Header>
-        <p>ID of the movie {id}</p>
+        <h2>DETAIL PAGE: {title}</h2>
+
+        <div className='detailMovie'>
+          
+          <div className='detailMovie-image'>
+            <img src={`https://image.tmdb.org/t/p/w500${poster_path}`} />
+          </div>
+
+          <div className='detailMovie-content'>
+            <p><strong>Argument:</strong> {overview}</p>
+            <p><strong>Valoration :</strong> {vote_average}</p>
+            { homepage && <a href={homepage}>Official Site</a> }
+          </div>
+
+        </div>
+
+        <span className='centerBack'>
+          <Link to="/">Back to home</Link>
+        </span>
+
       </div>
     );
   }
